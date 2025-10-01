d8b7677bb96c302c0c5b6164f70d72465cb631b8
maunsolsson <Pontus@notvallens.se>
2025-10-01 09:41:00 +0200

    Testdata implimenterad

---
+++ R/script for scraping wiki_women.R
@@ -0,0 +1,16 @@
+library(rvest)
+library(dplyr)
+library(stringr)
+library(jsonlite)
+
+test <- read_html(x = "https://en.wikipedia.org/wiki/List_of_women_in_statistics")
+
+test2 <- test %>% html_element("div.mw-content-ltr.mw-parser-output") %>% html_nodes("ul") %>%
+  html_nodes("a") %>% html_attr("href")
+
+test2 <- str_subset(test2, "^/wiki/")
+test2 <- str_replace(test2, pattern = "/wiki/", replacement = "")
+
+women_in_statistics <- test2
+
+saveRDS(women_in_statistics, file = "women_in_statistics.rds")
